5f06399b1a0a23fadbc3af25a66d36ba566c409b
Anderson Gabriel <gabrielvalencasa98@gmail.com>
2024-10-22 21:17:36 -0300

    Quebra de texto adicionada na section projects

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -91,13 +91,13 @@ export default function MyProjectsSection() {
                 alt={selectedProject.title}
                 className="w-full h-64 object-cover rounded-lg mb-4"
               />
-              <h3 className="text-2xl font-semibold mb-2">
+              <h3 className="text-2xl break-words font-semibold mb-2">
                 {selectedProject.title}
               </h3>
-              <p className="text-gray-300 mb-4">
+              <p className="text-gray-300 break-words mb-4">
                 {selectedProject.description}
               </p>
-              <div className="flex flex-wrap gap-2">
+              <div className="flex flex-wrap break-words gap-2">
                 {selectedProject.technologies.map((tech, index) => (
                   <span
                     key={index}
